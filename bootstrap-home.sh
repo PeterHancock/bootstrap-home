e1af89e68bc6810ce56f1c36503bde01eee67d39
Peter Hancock <peter.hancock@gmail.com>
2014-02-11 11:07:13 +0000

    GVM

--- bootstrap-home.sh
+++ bootstrap-home.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-#sudo apt-get -y update
-#sudo apt-get -y upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install git
-sudo apt-get install rubygems
+sudo apt-get -y install rubygems
+
+cd ~
 
 #dotfiles
 sudo gem install homesick
 homesick clone https://github.com/PeterHancock/dotfiles.git
 homesick symlink dotfiles
+
+# TODO Java
+
+# Groovy and friends
+curl -s api.gvmtool.net | bash
 
